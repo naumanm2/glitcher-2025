1b2d0ec96d34b12f2332b0f101e1ad16f85e4856
naumanm2 <max.naumanen@aalto.fi>
2025-02-11 08:46:22 +0200

    add tphm

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import TextWithLead from "./components/text-with-lead";
 import Spacer from "./components/spacer";
 import CardCollection from "./components/card-collection";
@@ -7,8 +6,10 @@ import Hero from "./components/hero";
 import TextParagraph from "./components/text-paragraph";
 import CTAdisplay from "./components/cta-display";
 import LogoRow from "./components/logo-row";
-import Bloodbath from "../public/general/bloodbath.jpg";
-import BloodbathBlur from "../public/general/bloodbath-blur.jpg";
+// import Bloodbath from "../public/general/bloodbath.jpg";
+// import BloodbathBlur from "../public/general/bloodbath-blur.jpg";
+import ThePornHorrorMusicalTwo from "../../public/general/tphm.jpg";
+import ThePornHorrorMusicalTwoBlur from "../../public/general/tphm-blur.jpg";
 import HowToLiveTogether from "../public/general/how-to-live-together.jpg";
 import HowToLiveTogetherBlur from "../public/general/how-to-live-together-blur.jpg";
 
@@ -27,18 +28,26 @@ export default function Home() {
 					image={HowToLiveTogether}
 					imageBlurData={HowToLiveTogetherBlur}
 					imageAltText=""
-          paragraph="10.10.2024 – 10.01.2027"
+					paragraph="10.10.2024 – 10.01.2027"
 					headline="How to Live Together"
 					address="/shows/how-to-live-together"
 				/>
 				<Card
-					image={Bloodbath}
-					imageBlurData={BloodbathBlur}
-					imageAltText=""
-					headline="Bloodbath"
+					image={ThePornHorrorMusicalTwo}
+					imageBlurData={ThePornHorrorMusicalTwoBlur}
+					imageAltText={""}
+					headline={"The Porn Horror Musical 2"}
 					paragraph="10.10.2024 – 10.01.2027"
-					address="/shows/bloodbath"
+					address="/shows/the-porn-horror-musical-2"
 				/>
+				{/* <Card
+						image={Bloodbath}
+						imageBlurData={BloodbathBlur}
+						imageAltText=""
+						headline="Bloodbath"
+						paragraph="10.10.2024 – 10.01.2027"
+						address="/shows/bloodbath"
+					/> */}
 			</CardCollection>
 			<Spacer />
 			<TextParagraph
