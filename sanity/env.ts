c6262079f2c10aaa9589fc063df4012fb9bc05bd
naumanm2 <max.naumanen@aalto.fi>
2025-02-25 11:30:21 +0200

    sanity_dataset –> sanity_studio_dataset

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,14 +1,14 @@
 export const apiVersion =
-  process.env.SANITY_API_VERSION || '2025-02-19'
+  process.env.SANITY_STUDIO_API_VERSION || '2025-02-19'
 
 export const dataset = assertValue(
-  process.env.SANITY_DATASET,
-  'Missing environment variable: SANITY_DATASET'
+  process.env.SANITY_STUDIO_DATASET,
+  'Missing environment variable: SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.SANITY_PROJECT_ID,
-  'Missing environment variable: SANITY_PROJECT_ID'
+  process.env.SANITY_STUDIO_PROJECT_ID,
+  'Missing environment variable: SANITY_STUDIO_PROJECT_ID'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
