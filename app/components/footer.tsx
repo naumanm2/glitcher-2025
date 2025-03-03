6126375df45ddafa620aff9c99517ffef72bae02
@max.naumanen <max.naumanen@tbwa.fi>
2025-03-03 08:04:18 +0200

    change phone to 'tel:'

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,67 +6,74 @@ import urlFor from "../../utils/image-builder";
 import parsePhone from "../../utils/parse-phone";
 
 export default function Footer({ content }: { content: GENERAL_QUERYResult }) {
-	return (
-		<footer>
-			<div className="bg-foreground rounded-3xl flex flex-col gap-16 md:gap-16 text-white p-4 md:p-8">
-				<div className="flex md:flex-row flex-col w-full justify-between">
-					<div className="flex flex-row md:gap-6 pb-4">
-						<Link
-							href="/shows"
-							className="p-2 font-medium hover:underline cursor-pointer">
-							Shows
-						</Link>
-						<Link
-							href="/about-us"
-							className="p-2 font-medium hover:underline cursor-pointer">
-							About us
-						</Link>
-					</div>
-					<div className="flex flex-col-reverse gap-0 items-start">
-						<div className="">
-							<p className="p-2 hover:underline">
-								<a href={`mailto:${content?.email || "mailto:info@glitcher.info"}`}>
-									{content?.email || "info@glitcher.info"}
-								</a>
-							</p>
-							<p className="p-2 hover:underline">
-								<a href={`mailto:${content?.phone || "tel:+358 45 136 4006"}`}>
-									{parsePhone(content?.phone) || "tel:+358 45 136 4006"}
-								</a>
-							</p>
-						</div>
-						<div className="flex flex-row gap-6">
-							{content?.socials &&
-								content.socials.map((item, index) => (
-									<Link
-										key={index}
-										rel="noopener noreferrer"
-										target="_blank"
-										href={item.url}
-										className="p-2 font-regular underline hover:opacity-85 transition-opacity duration-100 ease-in-out">
-										{item.social}
-									</Link>
-								))}
-						</div>
-					</div>
-				</div>
-				<div className="text-glitcherred w-full scale-[125%] md:scale-110 flex justify-center font-display [&>path]:fill-glitcherpink -my-[7vw] md:-my-[6vw]">
-					{content?.secondaryLogo && content.secondaryLogo.asset ? (
-						<Image
-							src={urlFor(content.secondaryLogo.asset).url()}
-							alt={content.secondaryLogo.alt || "Main image of Glitcher Collective"}
-							width={1080}
-							height={280}
-							className="w-[100vw]"
-						/>
-					) : (
-						<p>Failed to fetch logo.</p>
-					)}
-				</div>
-				<div className="w-full text-center p-2">
-					<p>cc. Glitcher 2025.</p>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      <div className="bg-foreground rounded-3xl flex flex-col gap-16 md:gap-16 text-white p-4 md:p-8">
+        <div className="flex md:flex-row flex-col w-full justify-between">
+          <div className="flex flex-row md:gap-6 pb-4">
+            <Link
+              href="/shows"
+              className="p-2 font-medium hover:underline cursor-pointer"
+            >
+              Shows
+            </Link>
+            <Link
+              href="/about-us"
+              className="p-2 font-medium hover:underline cursor-pointer"
+            >
+              About us
+            </Link>
+          </div>
+          <div className="flex flex-col-reverse gap-0 items-start">
+            <div className="">
+              <p className="p-2 hover:underline">
+                <a
+                  href={`mailto:${content?.email || "mailto:info@glitcher.info"}`}
+                >
+                  {content?.email || "info@glitcher.info"}
+                </a>
+              </p>
+              <p className="p-2 hover:underline">
+                <a href={`tel:${content?.phone || "tel:+358 45 136 4006"}`}>
+                  {parsePhone(content?.phone) || "+358 45 136 4006"}
+                </a>
+              </p>
+            </div>
+            <div className="flex flex-row gap-6">
+              {content?.socials &&
+                content.socials.map((item, index) => (
+                  <Link
+                    key={index}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={item.url}
+                    className="p-2 font-regular underline hover:opacity-85 transition-opacity duration-100 ease-in-out"
+                  >
+                    {item.social}
+                  </Link>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className="text-glitcherred w-full scale-[125%] md:scale-110 flex justify-center font-display [&>path]:fill-glitcherpink -my-[7vw] md:-my-[6vw]">
+          {content?.secondaryLogo && content.secondaryLogo.asset ? (
+            <Image
+              src={urlFor(content.secondaryLogo.asset).url()}
+              alt={
+                content.secondaryLogo.alt || "Main image of Glitcher Collective"
+              }
+              width={1080}
+              height={280}
+              className="w-[100vw]"
+            />
+          ) : (
+            <p>Failed to fetch logo.</p>
+          )}
+        </div>
+        <div className="w-full text-center p-2">
+          <p>cc. Glitcher 2025.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -10,103 +10,110 @@ import urlFor from "../../utils/image-builder";
 import parsePhone from "../../utils/parse-phone";
 
 interface menuOptions {
-	open: boolean;
-	setOpen: Dispatch<SetStateAction<boolean>>;
-	content: GENERAL_QUERYResult;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  content: GENERAL_QUERYResult;
 }
 
 const Menu = ({ open, setOpen, content }: menuOptions) => {
-	return (
-		<div className="w-full h-full fixed top-0 left-0 -z-10 bg-glitcherpink p-2">
-			<div className="flex flex-col h-full justify-end gap-6">
-				<div className="flex flex-row justify-between">
-					<div className="flex flex-col self-end">
-						<Link
-							href="/shows"
-							onClick={() => setOpen(!open)}
-							className="font-medium text-2xl underline p-2 hover:underline">
-							Shows
-						</Link>
-						<Link
-							href="/about-us"
-							onClick={() => setOpen(!open)}
-							className="font-medium text-2xl underline p-2 hover:underline">
-							About us
-						</Link>
-					</div>
-					<div className="flex flex-row gap-6 [writing-mode:tb] underline-offset-2 -scale-100">
-						{content?.socials &&
-							content.socials.map((item, index) => (
-								<div
-									key={index}
-									className="p-2 font-regular underline hover:opacity-85 transition-opacity duration-100 ease-in-out">
-									<Link rel="noopener noreferrer" target="_blank" href={item.url}>
-										{item.social}
-									</Link>
-								</div>
-							))}
-					</div>
-				</div>
-				<div className="">
-					<p className="p-2 hover:underline">
-						<a href={`mailto:${content?.email || "mailto:info@glitcher.info"}`}>
-							{content?.email || "info@glitcher.info"}
-						</a>
-					</p>
-					<p className="p-2 hover:underline">
-						<a href={`mailto:${content?.phone || "tel:+358 45 136 4006"}`}>
-							{parsePhone(content?.phone) || "tel:+358 45 136 4006"}
-						</a>
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-full fixed top-0 left-0 -z-10 bg-glitcherpink p-2">
+      <div className="flex flex-col h-full justify-end gap-6">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col self-end">
+            <Link
+              href="/shows"
+              onClick={() => setOpen(!open)}
+              className="font-medium text-2xl underline p-2 hover:underline"
+            >
+              Shows
+            </Link>
+            <Link
+              href="/about-us"
+              onClick={() => setOpen(!open)}
+              className="font-medium text-2xl underline p-2 hover:underline"
+            >
+              About us
+            </Link>
+          </div>
+          <div className="flex flex-row gap-6 [writing-mode:tb] underline-offset-2 -scale-100">
+            {content?.socials &&
+              content.socials.map((item, index) => (
+                <div
+                  key={index}
+                  className="p-2 font-regular underline hover:opacity-85 transition-opacity duration-100 ease-in-out"
+                >
+                  <Link
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={item.url}
+                  >
+                    {item.social}
+                  </Link>
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="">
+          <p className="p-2 hover:underline">
+            <a href={`mailto:${content?.email || "mailto:info@glitcher.info"}`}>
+              {content?.email || "info@glitcher.info"}
+            </a>
+          </p>
+          <p className="p-2 hover:underline">
+            <a href={`tel:${content?.phone || "tel:+358 45 136 4006"}`}>
+              {parsePhone(content?.phone) || "+358 45 136 4006"}
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function Header({ content }: { content: GENERAL_QUERYResult }) {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	return (
-		<nav className=" flex items-center justify-between fixed w-[calc(100%-16px*2)] md:w-[calc(100%-29px*2)] pt-2 md:pt-8 z-20 top-0 ">
-			<div className="flex flex-row gap-6 max-md:hidden">
-				<Link href="/shows" className="font-medium p-2 hover:underline">
-					Shows
-				</Link>
-				<Link href="/about-us" className="font-medium p-2 hover:underline">
-					About us
-				</Link>
-			</div>
-			<div className="h-[84px] md:h-[92px]">
-				<Link href="/" onClick={() => open && setOpen(!open)}>
-					{content && (
-						<Image
-							src={urlFor(content.icon).url()}
-							alt={content.mainImage.alt}
-							width={96}
-							height={96}
-							style={{ height: "auto" }}
-						/>
-					)}
-				</Link>
-			</div>
-			<div className="max-md:hidden p-2">
-				<CTA
-					text="Contact us"
-					arrow={false}
-					address={`mailto:${content?.email || "info@glitcher.info"}`}
-				/>
-			</div>
-			<div className="md:hidden">
-				{!open ? (
-					<button onClick={() => setOpen(!open)}>MENU</button>
-				) : (
-					<div className="">
-						<button onClick={() => setOpen(!open)}>CLOSE</button>
-						<Menu setOpen={setOpen} open={open} content={content} />
-					</div>
-				)}
-			</div>
-		</nav>
-	);
+  return (
+    <nav className=" flex items-center justify-between fixed w-[calc(100%-16px*2)] md:w-[calc(100%-29px*2)] pt-2 md:pt-8 z-20 top-0 ">
+      <div className="flex flex-row gap-6 max-md:hidden">
+        <Link href="/shows" className="font-medium p-2 hover:underline">
+          Shows
+        </Link>
+        <Link href="/about-us" className="font-medium p-2 hover:underline">
+          About us
+        </Link>
+      </div>
+      <div className="h-[84px] md:h-[92px]">
+        <Link href="/" onClick={() => open && setOpen(!open)}>
+          {content && (
+            <Image
+              src={urlFor(content.icon).url()}
+              alt={content.mainImage.alt}
+              width={96}
+              height={96}
+              style={{ height: "auto" }}
+            />
+          )}
+        </Link>
+      </div>
+      <div className="max-md:hidden p-2">
+        <CTA
+          text="Contact us"
+          arrow={false}
+          address={`mailto:${content?.email || "info@glitcher.info"}`}
+        />
+      </div>
+      <div className="md:hidden">
+        {!open ? (
+          <button onClick={() => setOpen(!open)}>MENU</button>
+        ) : (
+          <div className="">
+            <button onClick={() => setOpen(!open)}>CLOSE</button>
+            <Menu setOpen={setOpen} open={open} content={content} />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
